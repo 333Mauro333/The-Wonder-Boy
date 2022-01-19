62d13f110161140b1b92fc243ad6f4de36d4ee59
Mauro Torres Velasquez <maurotv50@gmail.com>
2022-01-19 19:09:48 -0300

    Agregado de la clase GameControls (.h y .cpp)

---
+++ Wonder_boy/src/game_controls/game_controls.cpp
@@ -0,0 +1,16 @@
+#include "game_controls.h"
+
+
+namespace the_wonder_boy
+{
+	int GameControls::screenUp = Keyboard::Up;
+	int GameControls::screenDown = Keyboard::Down;
+	int GameControls::screenEnter = Keyboard::Enter;
+	int GameControls::screenReturn = Keyboard::BackSpace;
+
+	int GameControls::gameplayLeft = Keyboard::Left;
+	int GameControls::gameplayRight = Keyboard::Right;
+	int GameControls::gameplayJump = Keyboard::LAlt;
+	int GameControls::gameplayAttack = Keyboard::LControl;
+	int GameControls::gameplayPause = Keyboard::P;
+}
---
+++ Wonder_boy/src/game_controls/game_controls.h
@@ -0,0 +1,27 @@
+#ifndef GAME_CONTROLS_H
+#define GAME_CONTROLS_H
+
+#include "SFML/Window.hpp"
+
+using sf::Keyboard;
+
+
+namespace the_wonder_boy
+{
+	class GameControls
+	{
+	public:
+		static int screenUp;
+		static int screenDown;
+		static int screenEnter;
+		static int screenReturn;
+
+		static int gameplayLeft;
+		static int gameplayRight;
+		static int gameplayJump;
+		static int gameplayAttack;
+		static int gameplayPause;
+	};
+}
+
+#endif // !GAME_CONTROLS_H
